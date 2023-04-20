8d6fa72e34c08d3c1b7f7ccd9230f93de5035d48
john <jrborges52@gmail.com>
2023-04-19 17:30:03 -0700

    .

--- configs/db.config.js
+++ configs/db.config.js
@@ -1,22 +1,17 @@
 // Database connections
 const { Client } = require('pg');
 
-// const { HOSTNAME, PORT, DATABASE, USERNAME, PASSWORD } = process.env;
+const { connectionString } = process.env;
 
 const client = new Client({
-  HOSTNAME: 'dpg-ch034hrh4hstquodonlg-a',
-  PORT: 5432,
-  DATABASE: 'animepomosql',
-  USERNAME: 'animepomosql_user',
-  PASSWORD: 'jKDK8oEkwl1Q8JibFKbMj9sLPL7716KM',
-
-
-})
+  connectionString: connectionString,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
 
 client.connect().then(() => {
   console.log("Database connection established.")
-}).catch(e => {
-  console.error(e)
-})
+}).catch((err) => console.error('Error connecting to database', err));
 
 module.exports = client;
